008d2d4771b5761d285762fd12e9d10cc3c60b17
Felipe Gomez <lufegoro78@gmail.com>
2025-10-01 19:41:14 -0500

    resto

---
+++ app7.js
@@ -0,0 +1,5 @@
+alert("calcule el descuento del  10% del precio de un producto:");
+let producto=parseFloat(prompt("ingrese el valor del producto:"));
+let porcentaje= producto*0.10;
+let descuento =producto-porcentaje;
+alert("el precrio con el 10% del descuento es:"+" "+descuento );
